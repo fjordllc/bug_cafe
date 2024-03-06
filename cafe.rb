de47625d353107dd94e23eea62457c6d14b545b1
jepe-w <yoshino_k@vitallead.co.jp>
2024-03-06 11:02:09 +0900

    oder変数をそれぞれoder_foods,oder_drinksへ変更

--- cafe.rb
+++ cafe.rb
@@ -25,10 +25,10 @@ def take_order(menus)
 end
 
 puts 'bugカフェへようこそ！ご注文は？ 番号でどうぞ'
-order1 = take_order(DRINKS)
+order_drinks = take_order(DRINKS)
 
 puts 'フードメニューはいかがですか?'
-order2 = take_order(FOODS)
+order_foods = take_order(FOODS)
 
-total = DRINKS[order1][:price].to_i + FOODS[order2][:price].to_i
+total = DRINKS[order_drinks][:price].to_i + FOODS[order_foods][:price].to_i
 puts "お会計は#{total}円になります。ありがとうございました！"
